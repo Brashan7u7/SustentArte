db887b1b9f72f3237cf10766f73bb1b2aba6f9e3
Aldo Omar Bohorquez Bastidas <aldomarscr@gmail.com>
2024-03-21 15:59:52 -0600

    Creacion de dtos, servicios y cambio de puerto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PruebaModule } from './prueba/prueba.module';
+import { ProductosService } from './servicios/productos/productos.service';
 
 @Module({
   imports: [PruebaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProductosService],
 })
 export class AppModule {}
