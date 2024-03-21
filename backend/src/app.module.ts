68ae536b8c11d2793c307659565fcf5847c7cba6
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2024-03-21 15:55:13 -0600

    pasdsd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PruebaModule } from './prueba/prueba.module';
 
 @Module({
-  imports: [],
+  imports: [PruebaModule],
   controllers: [AppController],
   providers: [AppService],
 })
