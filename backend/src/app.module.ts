3383a157adc7da1855329e9c055774ec495f6281
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2024-03-22 14:19:52 -0600

    front

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PruebaModule } from './prueba/prueba.module';
+
 import { ProductosService } from './servicios/productos/productos.service';
 
 @Module({
-  imports: [PruebaModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService, ProductosService],
 })
