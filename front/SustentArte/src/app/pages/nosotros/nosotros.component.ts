e0bea43c60d592cf3eca061a94a89df3492b5864
TeslaBv <cruzfabianhiram@gmail.com>
2024-04-20 01:14:48 -0600

    Responsive

--- front/SustentArte/src/app/pages/nosotros/nosotros.component.ts
+++ front/SustentArte/src/app/pages/nosotros/nosotros.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FooterComponent],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
