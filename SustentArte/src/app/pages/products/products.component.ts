6cccdf7171273d20e228a40a0b06481ce5650ef2
Fabian <cruzfabianhiram@gmail.com>
2024-05-14 20:33:01 -0600

    Pagina Productos y  Modificaciones de required de Login y Register

--- SustentArte/src/app/pages/products/products.component.ts
+++ SustentArte/src/app/pages/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet,RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterOutlet,RouterModule,CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
