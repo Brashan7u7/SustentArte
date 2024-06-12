c0ace702a5295fbc251462a5cb5cb81561b2a3b6
TeslaBv <cruzfabianhiram@gmail.com>
2024-06-11 23:32:52 -0600

    correccion Productos y prroductoPrivate

--- SustentArte/src/app/pages/Compradores/carrito-productos/carrito-productos.component.ts
+++ SustentArte/src/app/pages/Compradores/carrito-productos/carrito-productos.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrito-productos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carrito-productos.component.html',
   styleUrl: './carrito-productos.component.css'
 })
