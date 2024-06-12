15d602bd4dbfd0a825d716452ba64d1c798bd7f2
CarlaArreola <147664189+CarlaArreola@users.noreply.github.com>
2024-06-11 23:37:53 -0600

    Productos_comprados

--- SustentArte/src/app/pages/Compradores/productos-comprados/productos-comprados.component.ts
+++ SustentArte/src/app/pages/Compradores/productos-comprados/productos-comprados.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-productos-comprados',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './productos-comprados.component.html',
   styleUrl: './productos-comprados.component.css'
 })
